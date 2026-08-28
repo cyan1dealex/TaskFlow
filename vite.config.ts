6b89a42ae0790214ec45b955289d7327d174708d
cyan1dealex <cyan1de.wot@mail.ru>
2026-08-28 17:52:21 +0300

    core: update config, aliases, prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+            '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+            '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+            '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+            '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+            '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
+        },
+    },
+});
